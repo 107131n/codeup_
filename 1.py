4ce53bdd4b206e9ac20ae7eae8f698d65bbc376b
107131n <ddolgi1206@icloud.com>
2023-03-23 22:22:26 +0900

    24th commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+# print("{name} is {age} years old".format(name="seunghyub", age="32"))
+# print("{item:>8}".format(item="hyub"))
+
+# seunghyub = 32
+# jaehyun = 30
+# print(f"seunghyub has {seunghyub} meats. jaehyun has {jaehyun} meats")
+
+# x = 7.0
+# print(f"x is a {type(x)} - number. Its value is {x}. The statement 'x is greater than 5' is {x>5}")
+
